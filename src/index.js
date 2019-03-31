437e4e1b743773e357c7c52aa8d943f05681f59e
Chris Dennett <chrisdennett@gmail.com>
2019-03-31 21:46:46 +0100

    Jelly begins!
    Hmmm, needs tentacles.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import posed from "react-pose";
+import { easing } from "popmotion";
 import "./styles.css";
 
-const startPos = { x: 70, y: 100 };
+// pose 1 - fat jelly head
+const startPos = { x: 50, y: 100 };
 const startControlPt = { x: 10, y: 100 };
 
 const middlePt = { x: 110, y: 10 };
 const middleControlPt = { x: 20, y: 10 };
 
-const endPos = { x: 160, y: 100 };
+const endPos = { x: 180, y: 100 };
 const endControlPt = { x: 210, y: 100 };
 
+// pose 2 - up and flat
 const start2Pos = { x: 10, y: 30 };
 const start2ControlPt = { x: 0, y: 20 };
 
@@ -20,6 +23,23 @@ const middle2ControlPt = { x: 20, y: 10 };
 
 const end2Pos = { x: 210, y: 30 };
 const end2ControlPt = { x: 220, y: 20 };
+
+// pose 3 - mid thrust position
+const start3Pos = { x: 20, y: 90 };
+const start3ControlPt = { x: 70, y: 60 };
+
+const middle3Pt = { x: 110, y: 10 };
+const middle3ControlPt = { x: 40, y: 10 };
+
+const end3Pos = { x: 200, y: 90 };
+const end3ControlPt = { x: 150, y: 60 };
+
+const path3 = ` M${start3Pos.x} ${start3Pos.y}
+                C ${start3ControlPt.x} ${start3ControlPt.y}, 
+                ${middle3ControlPt.x} ${middle3ControlPt.y}, 
+                ${middle3Pt.x} ${middle3Pt.y} 
+                S ${end3ControlPt.x} ${end3ControlPt.y}, 
+                ${end3Pos.x} ${end3Pos.y} z`;
 
 const path1 = ` M${startPos.x} ${startPos.y}
                 C ${startControlPt.x} ${startControlPt.y}, 
@@ -35,66 +55,38 @@ const path2 = ` M${start2Pos.x} ${start2Pos.y}
                 S ${end2ControlPt.x} ${end2ControlPt.y}, 
                 ${end2Pos.x} ${end2Pos.y} z`;
 
-const Path = posed.path({
-  path1: {
+const PathFramed = posed.path({
+  init: { d: path1 },
+  swim: {
     d: path1,
     transition: {
-      default: { type: "spring", stiffness: 90 }
-    }
-  },
-  path2: {
-    d: path2,
-    transition: {
-      default: { ease: "easeOut", duration: 500 }
-    }
-  }
-});
-
-const Box = posed.div({
-  hidden: {
-    opacity: 0.1,
-    scaleX: 0.1,
-    scaleY: 0.1,
-    transition: { duration: 200 }
-  },
-
-  visible: {
-    opacity: 1,
-    scaleX: 1,
-    scaleY: 1,
-    transition: {
-      opacity: { ease: "easeIn", duration: 500 },
-      scaleY: { ease: "easeOut", duration: 100, delay: 1000 },
-      default: { ease: "easeOut", duration: 100 }
+      type: "keyframes",
+      values: [path1, path2, path3, path1],
+      loop: Infinity,
+      easings: [easing.bounceInOut, easing.bounceInOut, easing.easeOut],
+      times: [0, 0.6, 0.85, 1],
+      duration: 2000
     }
   }
 });
 
 class Example extends React.Component {
-  state = { isVisible: true, path: "path1" };
-
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        isVisible: !this.state.isVisible,
-        path: this.state.path === "path1" ? "path2" : "path1"
-      });
-    }, 2000);
-  }
-
   render() {
-    const { isVisible, path } = this.state;
     return (
       <div style={{ background: "yellow" }}>
         <div style={{ padding: 20 }}>
           <svg width="600" height="600">
-            <g transform={"translate(0 200)"}>
-              <path d={path2} />
+            <g transform={"translate(0 0)"}>
+              <path fill={"none"} stroke={"black"} d={path1} />
+              <path fill={"none"} stroke={"black"} d={path2} />
+              <path fill={"none"} stroke={"black"} d={path3} />
             </g>
 
-            <Path pose={path} />
+            <g transform={"translate(300 0)"}>
+              {/* <PathFramed pose={isVisible ? "swim" : "hidden"} /> */}
+              <PathFramed initialPose="pre" pose="swim" />
+            </g>
           </svg>
-          <Box className="box" pose={isVisible ? "visible" : "hidden"} />
         </div>
       </div>
     );
